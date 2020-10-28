59f55fe251e9ae88e22755b852006e93556f83df
Wojciech Bogócki <wojciechbogocki@fastmail.com>
2020-10-28 17:33:14 +0800

    Add gitignore, remove compiled binaries

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2019
+
+Written in C in Taipei during December 2019. Day 17 is the last completed one.
--- day18/day18.c
+++ day18/day18.c
@@ -3,15 +3,87 @@
     ** Day 18 Part 1 **
     ** Wojciech Bogócki **
     ** 21 Dec 2019 Taipei **
+    ** 13 March 2020 Taipei **
 */
 
+#include <limits.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
+
+#define MAX_X 128
+#define MAX_Y 128
+#define MAX_PATH 512
+
+struct position
+{
+    int X, Y;
+};
+
+bool
+FindPosition(int Width, int Height, char* Map[], char Character, struct position* Position)
+{
+    for(int Y = 0;
+        Y < Height;
+        ++Y)
+    {
+        for(int X = 0;
+            X < Width;
+            ++X)
+        {
+            if(Map[X][Y] == Character)
+            {
+                Position->X = X;
+                Position->Y = Y;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool
+FindShortestPath(int Width, int Height, char* Map[], int KeyCount, char* Keys, struct position From, struct position To, int* Length)
+{
+    int Distances[MAX_X][MAX_Y] = {{0}};
+    struct position Path[MAX_PATH] = {0};
+    int PathLength = 0;
+
+    Path[PathLength++] = From;
+
+    for(;;)
+    {
+        struct position Current = Path[PathLength - 1];
+
+        if(Current.X > 0)
+        {
+            // Left
+        }
+        else if(Current.X < Width - 1)
+        {
+            // Right
+        }
+        else if(Current.Y > 0)
+        {
+            // Down
+        }
+        else if(Current.Y < Height - 1)
+        {
+            // Up
+        }
+        else
+        {
+            // Back
+        }
+    }
+
+    return false;
+}
 
 int
 main(void)
 {
-    char Map[128][128] = {{0}};
+    char Map[MAX_X][MAX_Y] = {{0}};
     int Width = 0;
     int Height = 0;
 
@@ -34,6 +106,20 @@ main(void)
         }
     }
     Width = strlen(Map[0]);
+
+    for(int Y = 0;
+        Y < Height;
+        ++Y)
+    {
+        for(int X = 0;
+            X < Width;
+            ++X)
+        {
+            putchar(Map[X][Y]);
+        }
+        putchar('\n');
+    }
+    fflush(stdout);
 
     // shortest distance from each key to each other key
     // doors needed before a key is unlocked
